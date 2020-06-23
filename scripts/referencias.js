e5db3bc06e95e9f675cd6b69537e20f21caba3c9
thaisaraujo <thais.araujo.hp@hotmil.com>
2020-06-23 03:15:16 -0300

    tamanho letra do software

--- scripts/referencias.js
+++ scripts/referencias.js
@@ -7,23 +7,22 @@
 */
 
 
-var Referencias = new Array(
-	{
-		link: 'ref_estat',
-		titulo: 'Referência 1:',
-		texto: [
-			'Você pode estudar outras situações que envolvem os mesmos conteúdos abordados aqui  no software <a href=" http://www.uff.br/cdme/distfreq/distfreq-html/dfreqcont.html" target="_blank">"Distribuições de Frequência e Seus Gráficos"</a>, desenvolvido pela Universidade federal Fluminense.'
-		]
-	}
-	/*
-		link: 'ref_estat',
-		titulo: 'Veja um trecho da matéria retirada do jornal “Cidadão do Povo” desta última terça-feira:',
-		texto: [
-			'Pesquisas recentes do IBGE – Instituto Brasileiro de Geografia e Estatística – coletou informações sobre a renda mensal das famílias do país. Os dados estão representados no histograma abaixo.',
-			'<br><img src="imgs/grafico.jpg" style="width:350px;"/>',
-			'A comparação desses dados com os de outros países mostra que o Brasil está entre as piores distribuições de renda. Para montar o ranking de distribuição de renda comparou-se a porcentagem da renda total do país que fica com a metade mais pobre da população.',
-			'O Brasil tem apenas 14,4% da renda nacional total com metade da população. Isto é, a outra metade, mais rica, detêm 85,6% de toda a renda!'
-		]
-	}
-	*/
+var Referencias = new Array({
+        link: 'ref_estat',
+        titulo: 'Referência 1:',
+        texto: [
+            'Você pode estudar outras situações que envolvem os mesmos conteúdos abordados aqui  no software <a href=" http://www.cdme.im-uff.mat.br/distfreq/distfreq-html/dfreqint.html" target="_blank">"Distribuições de Frequência e Seus Gráficos"</a>, desenvolvido pela Universidade federal Fluminense.'
+        ]
+    }
+    /*
+    	link: 'ref_estat',
+    	titulo: 'Veja um trecho da matéria retirada do jornal “Cidadão do Povo” desta última terça-feira:',
+    	texto: [
+    		'Pesquisas recentes do IBGE – Instituto Brasileiro de Geografia e Estatística – coletou informações sobre a renda mensal das famílias do país. Os dados estão representados no histograma abaixo.',
+    		'<br><img src="imgs/grafico.jpg" style="width:350px;"/>',
+    		'A comparação desses dados com os de outros países mostra que o Brasil está entre as piores distribuições de renda. Para montar o ranking de distribuição de renda comparou-se a porcentagem da renda total do país que fica com a metade mais pobre da população.',
+    		'O Brasil tem apenas 14,4% da renda nacional total com metade da população. Isto é, a outra metade, mais rica, detêm 85,6% de toda a renda!'
+    	]
+    }
+    */
 );
